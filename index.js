3860b8bb79664a71044403f5eff7526b96d85afb
MNH-web <47294100+MNH-web@users.noreply.github.com>
2019-06-26 15:30:21 +0100

    Add files via upload
    add link to news letter and update js

--- index.js
+++ index.js
@@ -3,12 +3,32 @@ $(".cssimg").click(function(){
 
 });
 
+
 $(".skills-title").click(function(){
     $(".Skills-show").fadeToggle();
+
+      
+  });
+
+$(".portfolio-title").click(function(){
+ $(".Portfolio-show").fadeToggle();
+ ;
+});
+
+
+  $(".htmlimg").click(function(){
+    $(".html-info").toggle("html-show");
   
   });
 
 
+
+
+
+  $(".javascriptimg").click(function(){
+    $(".java-info").toggle("java-show");
+  
+  });
 
 
 
